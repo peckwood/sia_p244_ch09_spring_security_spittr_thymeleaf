6d3f7f05b22d46aa740d01b2b05af24b3caf26eb
raiden <peckwood@users.noreply.github.com>
2017-07-22 11:02:35 +0800

    base Spittr web project without security working

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Changes
+
+- pom.xml added:
+  - `org.springframework.security:spring-security-config`
+  - `org.springframework.security:spring-security-web`
+  - `org.thymeleaf.extras.thymeleaf-extras-springsecurity3`
+  - `org.springframework.ldap.spring-ldap-core`
+  - ​
---
+++ src/main/java/spittr/web/WebConfig.java
@@ -0,0 +1,51 @@
+package spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+	static{
+		System.out.println("WebConfig loaded");
+	}
+
+  @Bean
+  public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine);
+    viewResolver.setCharacterEncoding("UTF-8");//needed for rendering Asian characters
+    return viewResolver;
+  }
+  @Bean
+  public SpringTemplateEngine templateEngine(TemplateResolver templateResolver) {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver);
+    return templateEngine;
+  }
+
+  @Bean
+  public TemplateResolver templateResolver() {
+    TemplateResolver templateResolver = new ServletContextTemplateResolver();
+    templateResolver.setPrefix("/WEB-INF/views/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode("HTML5");
+    return templateResolver;
+  }
+    
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+  
+}
